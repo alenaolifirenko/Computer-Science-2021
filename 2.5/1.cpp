92b854357db559c3553c9e81baea47fc2ed75b9f
alena <alenaolifirenko@rambler.ru>
2021-03-09 10:40:01 +0300

    Seminar 5

---
+++ 2.5/1.cpp
@@ -0,0 +1,34 @@
+﻿#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <locale>
+#include <sstream>
+
+#include <Windows.h> 
+
+
+int main(int argc, char** argv)
+{
+
+	SetConsoleCP(CP_UTF8); 
+	SetConsoleOutputCP(CP_UTF8); 
+
+	long double value;
+	std::string input;
+	std::getline(std::cin, input);
+	std::istringstream in(input);
+	//std::istringstream in("1234 EUR");
+	in.imbue(std::locale("de_BE.UTF-8"));
+	in >> std::get_money(value);
+
+	value *= 88.93;
+	
+	std::cout.imbue(std::locale("ru_RU.UTF-8"));
+	std::cout << std::showbase << std::put_money(value, false) << '\n';
+
+
+	system("pause");
+
+	return EXIT_SUCCESS;
+}
---
+++ 2.5/2.cpp
@@ -0,0 +1,25 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <regex>
+#include <string>
+int main(int argc, char** argv)
+{
+	std::string data = R"(vishalchovatiya@ymail.com vishalchovatiya@mail.comhgdvjhgsyfgbxzhg
+	,,....  hjgd@@mail.ruhf___&&&63j@rambler.ru)";
+
+	std::regex pattern_1(R"(\w+@(?:mail|rambler)+\.(?:com|ru))");
+	
+
+	std::copy(
+		std::sregex_token_iterator(data.begin(), data.end(), pattern_1),
+		std::sregex_token_iterator(),
+		std::ostream_iterator < std::string >(std::cout, "\n"));
+
+	std::cout << std::endl;
+
+
+	system("pause");
+
+	return EXIT_SUCCESS;
+}
